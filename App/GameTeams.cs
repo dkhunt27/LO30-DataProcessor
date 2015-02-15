ea0e412225aef9b5ccac991395bbb36a8829a743
dkhunt27 <huntd@valassis.com>
2015-02-15 09:56:35 -0500

    initial checkin

---
+++ App/GameTeams.cs
@@ -0,0 +1,94 @@
+﻿using LO30.Data;
+using LO30.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+  public class GameTeams
+  {
+    public void SaveOrUpdateGameTeams(Lo30Context context, Lo30ContextService lo30ContextService, string folderPath, int startingGameIdToProcess, int endingGameIdToProcess)
+    {
+     /* ProcessingResult pr = new ProcessingResult();
+      pr.TableName = "GameTeams";
+      pr.StartTime = DateTime.Now;
+
+      dynamic parsedJson = _accessDatabaseService.ParseObjectFromJsonFile(folderPath + "Games.json");
+      int count = parsedJson.Count;
+
+      Print("SaveOrUpdateGameTeams: Access records to process:" + count);
+
+      int countSaveOrUpdated = 0;
+      for (var d = 0; d < parsedJson.Count; d++)
+      {
+        if (d % 100 == 0) { Print("SaveOrUpdateGameTeams: Access records processed:" + d); }
+        var json = parsedJson[d];
+
+        int gameId = json["GAME_ID"];
+
+        if (gameId >= startingGameIdToProcess && gameId <= endingGameIdToProcess)
+        {
+          int homeTeamId, awayTeamId;
+
+          /*switch (gameId)
+          {
+            case 3276:
+              homeTeamId = 322;
+              awayTeamId = 321;
+              break;
+            case 3277:
+              homeTeamId = 324;
+              awayTeamId = 323;
+              break;
+            case 3278:
+              homeTeamId = 326;
+              awayTeamId = 325;
+              break;
+            case 3279:
+              homeTeamId = 328;
+              awayTeamId = 327;
+              break;
+            case 3316:
+              homeTeamId = 328;
+              awayTeamId = 327;
+              break;
+            case 3317:
+              homeTeamId = 326;
+              awayTeamId = 325;
+              break;
+            case 3318:
+              homeTeamId = 324;
+              awayTeamId = 323;
+              break;
+            case 3319:
+              homeTeamId = 322;
+              awayTeamId = 321;
+              break;
+            default:
+              homeTeamId = json["HOME_TEAM_ID"];
+              awayTeamId = json["AWAY_TEAM_ID"];
+              break;
+          };*/
+      /*
+          homeTeamId = json["HOME_TEAM_ID"];
+          awayTeamId = json["AWAY_TEAM_ID"];
+
+          var gameTeam = new GameTeam(gid: gameId, ht: true, stid: homeTeamId);
+          countSaveOrUpdated = countSaveOrUpdated + lo30ContextService.SaveOrUpdateGameTeam(gameTeam);
+
+          gameTeam = new GameTeam(gid: gameId, ht: false, stid: awayTeamId);
+
+          countSaveOrUpdated = countSaveOrUpdated + lo30ContextService.SaveOrUpdateGameTeam(gameTeam);
+        }
+      }
+
+      Print("SaveOrUpdateGameTeams: GameTeams Count: " + context.GameTeams.Count() + " SaveOrUpdated:" + countSaveOrUpdated);
+      var diffFromLast = DateTime.Now - last;
+      Print("SaveOrUpdateGameTeams: TimeToProcess: " + diffFromLast.ToString());*/
+    }
+
+  }
+}
---
+++ App/ProcessingResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+  /*public class ProcessingResults
+  {
+    public List<ProcessingResult> Results { get; set; }
+  }
+
+  public class ProcessingResult
+  {
+    public string TableName { get; set; }
+    public int ProcessedCount { get; set; }
+    public int SavedOrUpdatedCount { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+  }*/
+}
